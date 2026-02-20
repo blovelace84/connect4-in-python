1cb9105d140afc551c5ec1fc18eb51d3687f53ee
Brittani Lovelace <blovelace84@gmail.com>
2026-02-20 10:09:40 -0500

    update app with flask and troubleshooting flask to run my app

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+ROWS = 6
+COLS = 7
+board = [[" "]*COLS for _ in range(ROWS)]
+player = 1
+
+def drop_piece(col):
+    global player
+    for r in reversed(range(ROWS)):
+        if board[r][col] == " ":
+            board[r][col] =  "X" if player == 1 else "0"
+            player = 2 if player == 1 else 1
+            break
+
+@app.route('/')
+def home():
+    return render_template("index.html", board=board)
+
+@app.route("/move/<int:col>")
+def move(col):
+    drop_piece(col)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
